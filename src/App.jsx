56e7f348e5085b5dcaca4e7b21eab98b1973d40b
fleuraje <127205495+fleuraje@users.noreply.github.com>
2023-06-30 21:43:17 -0400

    added functionality to color picker to change color in nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react'
 // Write your Color component here
+const Color = ({color, setSelectedColor,selected}) => {
+  return (
+    <div 
+      className={`${color} ${selected ? "selected" : ""}`} 
+      onClick={() => setSelectedColor(color)} 
+     ></div>
+  )
+}
+
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("") 
   return (
+    
     <div id="container">
       <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
-      </div>
-      <div id="colors-list">{/* colors go here */}</div>
+  <div>Currently selected: </div>
+  <div className={selectedColor}>{selectedColor}</div>
+</div>
+<div id="colors-list">
+  <Color color="red" setSelectedColor={setSelectedColor} selected={selectedColor==="red"}/>
+  <Color color="blue" setSelectedColor={setSelectedColor} selected={selectedColor==="blue"}/>
+  <Color color="green" setSelectedColor={setSelectedColor} selected={selectedColor==="green"}/>
+</div>
+
     </div>
   );
 };
